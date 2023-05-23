4ab0d5f302e7f6e7c32b4b1c8876b06819b8ef30
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-23 13:41:50 -0300

    implementando rotas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./Paginas/Inicio";
 import SobreMim from "./Paginas/SobreMim";
 
@@ -7,7 +7,11 @@ import SobreMim from "./Paginas/SobreMim";
 function App() {
   return (
     <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<SobreMim />}/>
+        <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
     </BrowserRouter>
 
   );
