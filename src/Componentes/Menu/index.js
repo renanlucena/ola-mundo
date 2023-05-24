967e2965a193e2bb31ab2134bd02213be3a90a07
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-24 09:59:05 -0300

    removendo importação desnecessária

--- src/Componentes/Menu/index.js
+++ src/Componentes/Menu/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Menu.module.css';
-import { Link, useLocation } from 'react-router-dom';
 import MenuLink from '../MenuLink';
 
 export default function Menu() {
