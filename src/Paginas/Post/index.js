089c500c3ded2abe12b7903d4fc562d6421d91dd
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-24 14:27:44 -0300

    implementando as páginas dinâmicas dos posts

--- src/Paginas/Post/index.js
+++ src/Paginas/Post/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import posts from 'json/posts.json';
+import PostModelo from 'Componentes/PostModelo';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import './Post.css';
 
 export default function Post() {
-    const parametros = useParams();
+  const parametros = useParams();
 
-    
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
+
+
   return (
-    <div>Post</div>
+    <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+          {post.texto}
+        </ReactMarkdown>
+      </div>
+    </PostModelo>
   )
 }
--- src/routes.js
+++ src/routes.js
@@ -14,10 +14,10 @@ function AppRoutes() {
 
       <Menu />
       <Routes>
-        <Route path="/" element = {<PaginaPadrao />}>
+        <Route path="/" element = {<PaginaPadrao/>}>
           <Route index element={<Inicio/>}/>
-          <Route path="sobremim" element={<SobreMim />}/>
-          <Route path="posts/:id" element={<Post />}/>
+          <Route path="sobremim" element={<SobreMim/>}/>
+          <Route path="posts/:id" element={<Post/>}/>
         </Route>
 
 
